be6546bdec07eff792579a356b993f83304ad3db
Diego Velasquez <contacto@diegovelasquez.co>
2024-01-28 00:07:25 -0500

    Add pages to functionality to app

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -1,30 +1,44 @@
+import { useState } from "react";
 import { json } from "@remix-run/node";
 import { Link, Outlet, useLoaderData, useRouteError } from "@remix-run/react";
+import { Provider as AppBridgeReactProvider } from "@shopify/app-bridge-react";
 import polarisStyles from "@shopify/polaris/build/esm/styles.css";
+import { DiscountProvider } from "../components/providers/DiscountProvider";
 import { boundary } from "@shopify/shopify-app-remix/server";
 import { AppProvider } from "@shopify/shopify-app-remix/react";
+
 import { authenticate } from "../shopify.server";
 
 export const links = () => [{ rel: "stylesheet", href: polarisStyles }];
 
-export const loader = async ({ request }) => {
+export async function loader({ request }) {
   await authenticate.admin(request);
 
-  return json({ apiKey: process.env.SHOPIFY_API_KEY || "" });
-};
+  const url = new URL(request.url);
+
+  return json({
+    apiKey: process.env.SHOPIFY_API_KEY,
+    host: url.searchParams.get("host"),
+  });
+}
 
 export default function App() {
-  const { apiKey } = useLoaderData();
+  const { apiKey, host } = useLoaderData();
+  const [config] = useState({ host, apiKey });
 
   return (
     <AppProvider isEmbeddedApp apiKey={apiKey}>
-      <ui-nav-menu>
-        <Link to="/app" rel="home">
-          Home
-        </Link>
-        <Link to="/app/additional">Additional page</Link>
-      </ui-nav-menu>
-      <Outlet />
+      <AppBridgeReactProvider config={config}>
+        <DiscountProvider>
+          <ui-nav-menu>
+            <Link to="/app" rel="home">
+              Home
+            </Link>
+            <Link to="/app/additional">Additional page</Link>
+          </ui-nav-menu>
+          <Outlet />
+        </DiscountProvider>
+      </AppBridgeReactProvider>
     </AppProvider>
   );
 }
--- extensions/product-discount/src/run.js
+++ extensions/product-discount/src/run.js
@@ -1,24 +1,52 @@
 // @ts-check
 import { DiscountApplicationStrategy } from "../generated/api";
 
+/**
+* @typedef {import("../generated/api").RunInput} RunInput
+* @typedef {import("../generated/api").FunctionRunResult} FunctionRunResult
+* @typedef {import("../generated/api").Target} Target
+* @typedef {import("../generated/api").ProductVariant} ProductVariant
+*/
+
+/**
+* @type {FunctionRunResult}
+*/
 const EMPTY_DISCOUNT = {
   discountApplicationStrategy: DiscountApplicationStrategy.First,
   discounts: [],
 };
 
+/**
+* @param {RunInput} input
+* @returns {FunctionRunResult}
+*/
 export function run(input) {
-  const targets = input.cart.lines
-  .filter((line) => line.quantity >= 2 &&
-    line.merchandise.__typename == "ProductVariant")
-  .map((line) => {
-    const variant = (line.merchandise);
-    return ({
+  // Define a type for your configuration, and parse it from the metafield
+  /**
+  * @type {{
+  *   quantity: number
+  *   percentage: number
+  * }}
+  */
+  const configuration = JSON.parse(
+    input?.discountNode?.metafield?.value ?? "{}"
+  );
+  if (!configuration.quantity || !configuration.percentage) {
+    return EMPTY_DISCOUNT;
+  }
 
-      productVariant: {
-        id: variant.id
-      }
+  const targets = input.cart.lines
+    // Use the configured quantity instead of a hardcoded value
+    .filter(line => line.quantity >= configuration.quantity &&
+      line.merchandise.__typename == "ProductVariant")
+    .map(line => {
+      const variant = /** @type {ProductVariant} */ (line.merchandise);
+      return /** @type {Target} */ ({
+        productVariant: {
+          id: variant.id
+        }
+      });
     });
-  });
 
   if (!targets.length) {
     console.error("No cart lines qualify for volume discount.");
@@ -31,7 +59,8 @@ export function run(input) {
         targets,
         value: {
           percentage: {
-            value: "10.0"
+            // Use the configured percentage instead of a hardcoded value
+            value: configuration.percentage.toString()
           }
         }
       }
